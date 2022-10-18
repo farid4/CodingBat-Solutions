fb170156e6049a83a07afdf1394ddf20d8175cbf
Farid Assfour <38664759+farid4@users.noreply.github.com>
2022-10-18 13:06:18 +0100

    Create cat_dog.py

---
+++ Python/String-2/cat_dog.py
@@ -0,0 +1,13 @@
+def cat_dog(str):
+  count1=0
+  count2=0
+  for i in range (len(str)-2):
+    if str[i:i+3] == "cat":
+      count1 +=1
+    if str[i:i+3] == "dog":
+      count2 +=1
+
+  if count1 == count2:
+    return True
+    
+  return False
