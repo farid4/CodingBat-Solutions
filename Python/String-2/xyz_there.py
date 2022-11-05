256a35800ad821b5f67de61787ab3b239d08db84
Farid Assfour <38664759+farid4@users.noreply.github.com>
2022-11-05 10:01:38 +0000

    Update xyz_there.py

--- Python/String-2/xyz_there.py
+++ Python/String-2/xyz_there.py
@@ -1,3 +1,6 @@
+# Return True if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.).
+# So "xxyz" counts but "x.xyz" does not.
+
 def xyz_there(str):
     if str[:3] == "xyz":
         return True
